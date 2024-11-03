d37ddea01004f6924372568142863eedfa1af2b3
Rumman Shahzad <76628969+rshaz2713@users.noreply.github.com>
2024-11-03 17:18:11 -0500

    Update .gitignore and fix a comment in processVideo.py

--- processVideo.py
+++ processVideo.py
@@ -36,7 +36,7 @@ def transcode_video_LQ(input_video_path, output_directory, base_filename):
     )
     return video_stream_output_path
 
-# Create a jpg stream @ 5fps
+# Create a jpg stream @ 1fps
 def extract_frames(input_video_path, output_directory, base_filename):
     jpg_output_directory = os.path.join(output_directory, f"{base_filename}_jpg_frames")
     os.makedirs(jpg_output_directory, exist_ok=True)
